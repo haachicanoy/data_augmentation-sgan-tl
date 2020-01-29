3e2343f1a908b9dd1076b6cc39332295396d4928
Harold <macbook@MacBook-Pro-de-MACBOOK.local>
2020-01-28 20:33:21 -0500

    New graphics for document

--- datasets_description/UTKFaces_EDA.R
+++ datasets_description/UTKFaces_EDA.R
@@ -17,12 +17,12 @@ ages$Age <- ages$Age %>% as.character() %>% as.numeric()
 
 g <- ages %>% ggplot2::ggplot(aes(x = Age, y = Frequency)) +
   ggplot2::geom_bar(stat = "identity", fill = "steelblue") +
-  ggplot2::xlab("Age (years)") +
-  ggplot2::ylab("Number of images") +
+  ggplot2::xlab("Edad (años)") +
+  ggplot2::ylab("Número de imágenes") +
   ggplot2::scale_x_continuous(breaks = seq(0, 120, 10)) +
   ggplot2::geom_rect(aes(xmin = 5, xmax = 18, ymin = -Inf, ymax = Inf), alpha = 0.005, fill = "red") +
   ggplot2::geom_rect(aes(xmin = 60, xmax = 116, ymin = -Inf, ymax = Inf), alpha = 0.005, fill = "red")
-ggplot2::ggsave(filename = "/Volumes/BACKUP/Documents/Data/UTKFaces/utkfaces_distribution.png", plot = g, device = "png", width = 8, height = 6, units = "in")
+ggplot2::ggsave(filename = "/Volumes/BACKUP/Documents/Data/UTKFaces/utkfaces_distribucion.png", plot = g, device = "png", width = 8, height = 6, units = "in")
 
 # Gender distribution 0:male 1:female
 files_spltd %>% purrr::map(2) %>% unlist() %>% as.numeric() %>% gtools::mixedsort() %>% table() %>% barplot(main = "Gender distribution")
