29909d7bead67d65dc343af82ef46ccdd9f84dc4
haachicanoy <harold22010@gmail.com>
2020-02-04 10:31:47 -0500

    Generating images

--- generate_images.R
+++ generate_images.R
@@ -5,31 +5,64 @@
 options(warn = -1, scipen = 999)
 suppressMessages(library(readr))
 
-data.frame(Source = c(c('paintings','portraits','pokemon','bedrooms','cats'),
-                      c('paintings','bedrooms','cats'),
-                      c('paintings','portraits','pokemon','bedrooms','cats')),
-           Target = c(rep('beans',5),
-                      rep('chars',3),
-                      rep('young_faces',5)))
-set.seed(1235)
-seeds <- sample(0:100000, 5, replace = F)
+cases2evaluate <- data.frame(Source = c(c('portraits','pokemon','paintings','cats','bedrooms'),
+                                        c('paintings','bedrooms','cats'),
+                                        c('portraits','pokemon','paintings','cats','bedrooms')),
+                             Target = c(rep('beans',5),
+                                        rep('chars',3),
+                                        rep('young_faces',5)))
+
+# Define working directory
+root <<- '/media/argos/DATA/HTH'
+setwd(root)
+setwd('./simulations')
 
 generate_images <- function(source_domain = "paintings",
                             target_domain = "young_faces",
-                            final_model = "network-final.pkl",
-                            seed = seeds[1],
+                            model = "network-final.pkl",
+                            number_images = 1000,
                             trunc_psi = 0.7)
 {
+  cat('>>> Set working directory\n')
   wk_dir <- paste0('stylegan_from_', source_domain, '_to_', target_domain)
-  # Move to specific dir
   setwd(paste0('./', wk_dir))
   
-  # Modify training_loop.py
-  generating_imgs <- readLines('./generating_imgs.py')
-  gen_imgs[] <- d # Model path
-  gen_imgs[] <- s # Directory for saving images
+  cat('>>> Set output directory\n')
+  if(!dir.exists(paste0('./generated_images/psi_',trunc_psi))){
+    dir.create(paste0('./generated_images/psi_',trunc_psi), recursive = T)
+  }
+  
+  cat('>>> Generate seeds\n')
+  set.seed(1235)
+  seeds <- sample(0:4000000, number_images, replace = F)
+  
+  for(i in 1:length(seeds)){
+    cat('>>> Set up generating images function\n')
+    file.copy(from = paste0(root, '/scripts/generating_imgs.py'), to = './generating_imgs.py', overwrite = T)
+    generating_imgs     <- readr::read_lines('./generating_imgs.py')
+    generating_imgs[11] <- gsub('in',paste0('./results/00000-sgan-custom-1gpu/',model),generating_imgs[11])
+    generating_imgs[16] <- gsub('123',seeds[i],generating_imgs[16])
+    outfile <- paste0('./generated_images/psi_',trunc_psi,'/img_',i,'_',seeds[i],'.jpg')
+    generating_imgs[20] <- gsub('out',outfile,generating_imgs[20])
+    readr::write_lines(generating_imgs, './generating_imgs.py')
+    system('python generating_imgs.py')
+  }
+  return(cat(paste0('Images generated for run: ',source_domain,' to ',target_domain,'\n')))
+  
+  root <<- '/media/argos/DATA/HTH'
+  setwd(root)
+  setwd('./simulations')
+  
 }
-
-gen_imgs <- readr::read_lines()
-gen_imgs[] <- d # Model path
-gen_imgs[] <- s # Directory for saving images
+generate_images(source_domain = 'paintings',
+                target_domain = 'young_faces',
+                model         = 'network-final.pkl',
+                number_images = 1000,
+                trunc_psi     = 0.7)
+for(j in c(1:10,12:13)){
+  generate_images(source_domain = cases2evaluate$Source[j],
+                  target_domain = cases2evaluate$Target[j],
+                  model         = 'network-final.pkl',
+                  number_images = 1000,
+                  trunc_psi     = 0.7)
+}
