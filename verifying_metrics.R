ede4b516fad3357e1ff269a0c89609ad80a1a8f1
Harold <macbook@MacBook-Pro-de-MACBOOK.local>
2021-07-20 15:01:40 -0500

    Add loss scores plot

--- verifying_metrics.R
+++ verifying_metrics.R
@@ -1,16 +1,15 @@
 options(warn = -1, scipen = 999)
 
-suppressPackageStartupMessages(library(googlesheets))
+suppressPackageStartupMessages(library(googlesheets4))
 suppressPackageStartupMessages(library(tidyverse))
 suppressPackageStartupMessages(library(RColorBrewer))
 
-eval <- googlesheets::gs_title("Loss metrics")
-
-metrics <- googlesheets::gs_read(eval)
+googlesheets4::gs4_auth()
+metrics <- googlesheets4::read_sheet('https://docs.google.com/spreadsheets/d/17XVqT8Oc6T7_zn3q6TWJHURcjhhotkuCEjP9mwVPR1c/edit#gid=0')
 metrics %>% dplyr::glimpse()
-metrics$Value <- metrics$Value %>% gsub(',','.',.) %>% as.numeric
+# metrics$Value <- metrics$Value %>% gsub(',','.',.) %>% as.numeric
 
-metrics %>%
+gg <- metrics %>%
   dplyr::filter(Metric %in% c('Loss scores real','Loss scores fake')) %>%
   dplyr::mutate(Metric = factor(Metric, levels = c('Loss scores real','Loss scores fake'))) %>%
   dplyr::group_by(Source, Target) %>%
@@ -20,7 +19,7 @@ metrics %>%
   ggplot2::geom_line(aes(linetype = Metric), size = 1.2) +
   ggplot2::facet_wrap(~Target) +
   ggplot2::xlim(0, 1000) +
-  ggplot2::xlab('Iterations') +
+  ggplot2::xlab('Iteration') +
   ggplot2::ylab('Loss scores') +
   ggplot2::geom_hline(yintercept = 0, linetype = 'dashed') +
   ggplot2::scale_color_brewer(palette = 'Set1') +
@@ -29,5 +28,5 @@ metrics %>%
                  axis.title = element_text(size = 20),
                  legend.text = element_text(size = 17),
                  legend.title = element_text(size = 20),
-                 strip.text.x = element_text(size = 20)) +
-  ggplot2::ggsave(filename = "/Volumes/BACKUP/Documents/Masters/Loss_scores.png", device = "png", width = 14, height = 8, units = "in")
+                 strip.text.x = element_text(size = 20))
+ggplot2::ggsave(filename = "/Volumes/BACKUP/Documents/Masters/Loss_scores.png", plot = gg, device = "png", width = 14, height = 8, units = "in")
